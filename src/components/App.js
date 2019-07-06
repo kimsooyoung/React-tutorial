f044a014f9c7936ac5dc9c1ddabf83e4e61a73f8
kimsooyoung <tge1375@naver.com>
2019-07-06 21:31:55 +0900

    df

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 class App extends React.Component{
+
     render(){
         return (
             <h1>
+                <button onClick={()=>{console.log('this is for test')}}>name</button>
                 Hello React!!!
             </h1>
         )
