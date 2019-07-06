bcbc25c474e75ededfacd36fb88369eede078c47
kimsooyoung <tge1375@naver.com>
2019-07-06 21:06:03 +0900

    Env setting done

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '127.0.0.1',
+        port: 4000,
+        contentBase: __dirname + '/public/',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
